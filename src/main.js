d09b9f4cae2561e7bdac99f5b992c3653017f3e4
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-25 01:05:28 +0800

    feat: implement user task assignment and display in main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import 'normalize.css';
+import { UserService } from './services/UserService.js';
+import { User } from './data/User.js';
+
+const bry = new User("Bryan");
+
+UserService.assignTask(undefined, bry);
+
+UserService.showTasks(bry);
