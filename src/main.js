0a94b0ad1a89dca19a65cc72e79edaa539f39c39
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-25 19:34:51 +0800

    feat: import Task and LocalRepository for task management functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import 'normalize.css';
 import { UserService } from './services/UserService.js';
 import { User } from './data/User.js';
+import { Task } from './data/Task.js';
+import { LocalRepository } from './repository/LocalRepository.js';
 
-const bry = new User("Bryan");
 
-UserService.assignTask(undefined, bry);
 
-console.log(bry.tasks);
