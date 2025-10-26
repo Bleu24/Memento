922626c3bfcf9f50e671ef39bb4ae239516e8937
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-26 21:21:59 +0800

    import subscriptions module in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import 'normalize.css';
+import './subscriptions.js';
 import { UserService } from './services/UserService.js';
 import { User } from './data/User.js';
 import { Task } from './data/Task.js';
