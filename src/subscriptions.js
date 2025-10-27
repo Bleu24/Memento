072586f4a9128b1a0bfb5c51188c25c25bd94b78
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-28 02:14:02 +0800

    fix: correct import statement for Notifications module and improve task handling in subscriptions

--- src/subscriptions.js
+++ src/subscriptions.js
@@ -1,12 +1,30 @@
-import { Notifications } from "./classes/Notifications";
+import { Notifications } from "./classes/Notifications.js";
+import { User } from "./classes/User.js";
+import { createExpHolder } from "./factories/expHolderFactory.js";
 
-const onTaskCreated = (data) => {
-    
+const onTaskCreated = (task) => {
+    if (!task) {
+        console.error("Invalid Task");
+        return;
+    }
+
+    if (task.priority === 'low') Object.assign(task, createExpHolder(10));
+    else if (task.priority === 'medium') Object.assign(task, createExpHolder(20));
+    else if (task.priority === 'high') Object.assign(task, createExpHolder(30));
+
+    console.log(task.getXP());
 }
 
 
 const onTaskCompleted = (data) => {
-    
+    const { task, user } = data;
+    const xp = task.getXP();
+
+    task.reduceXP(xp);
+    user.addXP(xp)
+
+    console.log(task.getXP());
+    console.log(user.getXP());
 }
 
 
