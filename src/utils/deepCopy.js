02c924402d7de7f21c1e4e5b16d82d41dd618514
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-30 20:04:28 +0800

    refactor function to recursion

--- src/utils/deepCopy.js
+++ src/utils/deepCopy.js
@@ -1,11 +1,10 @@
 export function deepCopy(obj) {
-    if (typeof obj === 'object') {
-        const copyObj = {};
-        for (const key in obj) {
-            copyObj[key] = obj[key];
-        }
-        return copyObj
+    if (typeof obj !== 'object') return obj;
+
+    let copyObj = {};
+    for (const key in obj) {
+        copyObj[key] = deepCopy(obj[key]);
     }
 
-    return;
+    return copyObj;
 }
