3109a3f2e5a97cd6f88a7ac594d3899f9e02863c
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-30 22:59:05 +0800

    created the home page

--- src/ui/pages/Home.js
+++ src/ui/pages/Home.js
@@ -1,5 +1,25 @@
 export const Home = (function () {
     const home = document.createElement("div");
-    
+    const heroBackground = document.createElement("div");
+    const heroContainer = document.createElement("div");
+    const heroText = document.createElement("h1");
+    const heroSubtext = document.createElement("p");
+    const heroButton = document.createElement("button");
 
+    home.className = "home";
+    heroBackground.className = "hero__bg";
+    heroContainer.className = "hero";
+    heroText.className = "hero__text";
+    heroSubtext.className = "hero__subtext";
+    heroButton.className = "hero__button";
+
+    heroBackground.appendChild(heroContainer);
+
+    heroContainer.appendChild(heroText);
+    heroContainer.appendChild(heroSubtext);
+    heroContainer.appendChild(heroButton);
+
+    home.appendChild(heroBackground);
+
+    return home;
 })();
