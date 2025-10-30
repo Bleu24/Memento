1709780165fbb3817b78201f3cad84adc3ca4ba9
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-30 22:57:57 +0800

    removed unused imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import 'normalize.css';
 import './styles.css';
 import './utils/subscriptions.js';
-import { UserService } from './services/UserService.js';
-import { User } from './data/User.js';
-import { Task } from './data/Task.js';
-import { LocalRepository } from './repository/LocalRepository.js';
 import { Home } from './ui/pages/Home.js';
 import { Nav } from './ui/components/Nav.js';
 
