09ff4c8d6d64671b4e078f39efa220ba8c98f2da
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-01 03:27:50 +0800

    add event listeners

--- src/main.js
+++ src/main.js
@@ -7,3 +7,19 @@ import { Nav } from './ui/components/Nav.js';
 
 document.body.appendChild(Nav);
 document.body.appendChild(Home);
+
+document.body.addEventListener("click", (e) => {
+    const selectedLink = e.target.closest(".nav__link");
+    if (!selectedLink) return;
+
+    const navLinks = document.querySelectorAll(".nav__link");
+
+    navLinks.forEach(link => {
+        if (link === selectedLink) {
+            link.classList.add("active");
+            renderPage(selectedLink.dataset.page);
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
