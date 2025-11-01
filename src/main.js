bf4be9c6f5f1df0a07260309e6e0972a9437f4df
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-01 16:03:32 +0800

    import renderPage and add pageIsActive check before render

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './styles.css';
 import './utils/subscriptions.js';
 import { Home } from './ui/pages/Home/Home.js';
 import { Nav } from './ui/components/Nav.js';
+import { renderPage } from './utils/renderPage.js';
 
 
 document.body.appendChild(Nav);
@@ -17,6 +18,10 @@ document.body.addEventListener("click", (e) => {
     navLinks.forEach(link => {
         if (link === selectedLink) {
             link.classList.add("active");
+            const pageIsActive = document.querySelector(`div[data-status="active"]`);
+            if (pageIsActive) {
+                document.body.removeChild(pageIsActive);
+            }
             renderPage(selectedLink.dataset.page);
         } else {
             link.classList.remove("active");
