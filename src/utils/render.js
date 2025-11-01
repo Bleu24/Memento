72775a4030ab09ecab96a72534756ce8a4062868
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-02 01:58:26 +0800

    no changes

---
+++ src/utils/render.js
@@ -0,0 +1,27 @@
+import { Home } from "../ui/pages/Home/Home.js";
+import { About } from "../ui/pages/About.js";
+import { Contact } from "../ui/pages/Contact.js";
+import { SignUp } from "../ui/pages/SignUp.js";
+
+export const render = (page) => {
+    if (!page || typeof page !== "string") {
+        throw new Error("Invalid page");
+    }
+    switch (page) {
+        case "home":
+            document.body.appendChild(Home);
+            break;
+        case "about":
+            document.body.appendChild(About);
+            break;
+        case "contact":
+            document.body.appendChild(Contact);
+            break;
+        case "sign up":
+            document.body.appendChild(SignUp);
+            break;
+        default:
+            document.body.appendChild(Home);
+            break;
+    }
+}
