501db8d4bcd91c70d2a6af3322243d120827889e
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-02 12:57:09 +0800

    add new div for input-label groupings

--- src/ui/pages/SignUp.js
+++ src/ui/pages/SignUp.js
@@ -9,9 +9,16 @@ export const SignUp = (function () {
     const nameInput = document.createElement("input");
 
     const submitButton = document.createElement("button");
+    const emailGroup = document.createElement("div");
+    const nameGroup = document.createElement("div");
+    const heading = document.createElement("h1");
+
+    signUp.dataset.status = "active";
 
     signUp.className = "signUp";
     form.className = "form";
+    emailGroup.className = "form__group";
+    nameGroup.className = "form__group";
 
     emailLabel.className = "form__label";
     nameLabel.className = "form__label";
@@ -24,6 +31,8 @@ export const SignUp = (function () {
     emailLabel.textContent = "Email";
     nameLabel.textContent = "Name";
     submitButton.textContent = "Sign Up";
+    heading.textContent = "Sign up now to Memento";
+    heading.className = "signUp__heading";
 
     emailInput.type = "email";
     nameInput.type = "text";
@@ -32,10 +41,12 @@ export const SignUp = (function () {
     emailInput.placeholder = "johndoe@example.com";
     nameInput.placeholder = "John Doe";
 
-    form.appendChild(emailLabel);
-    form.appendChild(emailInput);
-    form.appendChild(nameLabel);
-    form.appendChild(nameInput);
+    form.appendChild(emailGroup);
+    emailGroup.appendChild(emailLabel);
+    emailGroup.appendChild(emailInput);
+    form.appendChild(nameGroup);
+    nameGroup.appendChild(nameLabel);
+    nameGroup.appendChild(nameInput);
     form.appendChild(submitButton);
 
     emailInput.required = true;
@@ -50,6 +61,7 @@ export const SignUp = (function () {
     emailInput.name = "email";
     nameInput.name = "name";
 
+    signUp.appendChild(heading);
     signUp.appendChild(form);
 
     return signUp;
