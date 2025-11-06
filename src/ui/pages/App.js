c239654f29f56e082d2989e078acdd1713d6ae24
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-07 02:16:36 +0800

    add app page

---
+++ src/ui/pages/App.js
@@ -0,0 +1,11 @@
+export const App = (function () {
+    const app = document.createElement("div");
+
+    
+
+
+
+    app.dataset.page = "app";
+    app.dataset.status = "active";
+    return app;
+})(); 
