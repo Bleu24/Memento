2d55e627356547d8995e519e113e69893da2f012
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-07 02:16:53 +0800

    use preventDefault to keep browser from reloading

--- src/ui/pages/SignUp.js
+++ src/ui/pages/SignUp.js
@@ -72,6 +72,8 @@ export const SignUp = (function () {
 
 
     form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
         const formData = new FormData(e.target);
         const email = formData.get("email");
         const name = formData.get("name");
