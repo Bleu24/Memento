1220f7f32b97fdd7201df49cd674ed8012e741df
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-13 02:04:12 +0800

    refactor task component to a factory function for reusability.

--- src/ui/components/Tasks.js
+++ src/ui/components/Tasks.js
@@ -1,36 +1,65 @@
-import { UserService } from "../../services/UserService.js";
+import { createElement, Dot } from "lucide";
 
-export const Tasks = (function () {
-    const tasks = document.createElement("div");
+export function displayTasks(tasks) {
+    const tasksDiv = document.createElement("div");
     const heading = document.createElement("h2");
     const taskCounter = document.createElement("h6");
     const taskList = document.createElement("ul");
-    const loggedInUser = UserService.loadProfileFromStorage();
 
-    
-    tasks.className = "tasksContainer";
+
+    tasksDiv.className = "tasksContainer";
     heading.className = "tasksContainer__heading";
     taskCounter.className = "tasksContainer__taskCounter";
     taskList.className = "tasksContainer__taskList";
-        
+
     heading.textContent = "All Tasks";
 
-    UserService.getTasksForUser(loggedInUser).forEach(task => {
-        const taskItem = document.createElement("li");
-        taskItem.id = task.id;
-        
+
+    const prioMap = [
+        ['low', 'green'],
+        ['mid', 'blue'],
+        ['high', 'red']
+    ];
+
+
+    tasks.forEach(task => {
+        const taskItem = document.createElement('li');
+        const leftSideOfItem = document.createElement('div');
+        const rightSideOfItem = document.createElement('div');
+
+        //left side elements
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        const taskTitle = document.createElement('p');
+        taskTitle.textContent = task.title;
+
+        //right side elements
+        const dueDate = document.createElement('time');
+        dueDate.textContent = task.dueDate;
+
+        const prio = createElement(Dot);
+
+        for (const arr of prioMap) {
+            if (arr[0] === task.priority) {
+                prio.style.setProperty('stroke', arr[1]);
+            }
+        }
+
+        // append nodes to corresponding parents
+
+        leftSideOfItem.append(checkBox, taskTitle);
+        rightSideOfItem.append(dueDate, prio);
+
+        taskItem.append(leftSideOfItem, rightSideOfItem);
+
         taskList.appendChild(taskItem);
     });
 
 
-
-    
-
-
-    tasks.appendChild(heading);
-    tasks.appendChild(taskCounter);
-    tasks.appendChild(taskList);
+    tasksDiv.appendChild(heading);
+    tasksDiv.appendChild(taskCounter);
+    tasksDiv.appendChild(taskList);
 
 
-    return tasks;
-})();
+    return tasksDiv;
+}
