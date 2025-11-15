6a3bdc267d23cdf473c358b9b3a9af9ab93b06a8
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-15 22:35:12 +0800

    add dummy task array to simulate display of tasks

--- src/ui/components/MainPanel.js
+++ src/ui/components/MainPanel.js
@@ -1,8 +1,16 @@
+import { displayTasks } from "./Tasks.js";
+import { Task } from "../../classes/Task.js";
+
 export const MainPanel = (function () {
 
     const main = document.createElement('div');
-    
+    main.className = 'mainPanel';
 
+
+    const dummyTasks = [new Task(null, "Coding", "Write upload feature", "July 9, 2026", "high")];
+    const displayedTasks = displayTasks(dummyTasks);
+
+    main.appendChild(displayedTasks);
 
     return main;
 })();
