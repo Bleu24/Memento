6a8ba99444535a4d3d585152e00cf0e8795425d3
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-22 02:35:01 +0800

    feat: enhance MainPanel to display completed tasks alongside all tasks

--- src/ui/components/MainPanel.js
+++ src/ui/components/MainPanel.js
@@ -1,4 +1,4 @@
-import { displayTasks } from "./Tasks.js";
+import { displayTasks, displayCompleteTasks } from "./Tasks.js";
 import { Task } from "../../classes/Task.js";
 
 export const MainPanel = (function () {
@@ -6,11 +6,12 @@ export const MainPanel = (function () {
     const main = document.createElement('div');
     main.className = 'mainPanel';
 
-
     const dummyTasks = [new Task(null, "Coding", "Write upload feature", "July 9, 2026", "high")];
-    const displayedTasks = displayTasks(dummyTasks);
+    const displayedTasks = displayTasks("All Tasks", dummyTasks);
+    const displayedCompleteTasks = displayCompleteTasks("Completed Tasks", dummyTasks)
 
     main.appendChild(displayedTasks);
+    main.appendChild(displayedCompleteTasks);
 
     return main;
 })();
