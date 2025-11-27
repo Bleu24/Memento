752b003adf8bd8d81c318f888c395f623c986782
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-27 13:19:36 +0800

    fix: add handleAppHydration function and subscribe to app:hydrate event in App component

--- src/ui/pages/App.js
+++ src/ui/pages/App.js
@@ -1,6 +1,7 @@
 import { LeftPanel } from "../components/LeftPanel.js";
 import { AppNav } from "../components/AppNav.js";
 import { MainPanel } from "../components/MainPanel.js";
+import { Notifications } from "../../classes/Notifications.js";
 
 export const App = (function () {
     const app = document.createElement("div");
@@ -12,4 +13,10 @@ export const App = (function () {
     app.appendChild(LeftPanel.el);
     app.appendChild(MainPanel);
     return app;
-})(); 
+})();
+
+const handleAppHydration = (userData) => {
+    LeftPanel.render(userData);
+}
+
+Notifications.subscribe("app:hydrate", handleAppHydration);
