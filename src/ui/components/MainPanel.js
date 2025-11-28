53dbfd5d32a897641ebc80c71ea05fff2bb9c0c4
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-11-28 14:39:02 +0800

    feat: refactor MainPanel to render tasks based on user data

--- src/ui/components/MainPanel.js
+++ src/ui/components/MainPanel.js
@@ -1,17 +1,18 @@
 import { displayTasks, displayCompleteTasks } from "./Tasks.js";
 import { Task } from "../../classes/Task.js";
+import { UserService } from "../../services/UserService.js"
 
 export const MainPanel = (function () {
 
     const main = document.createElement('div');
     main.className = 'mainPanel';
 
-    const dummyTasks = [new Task(null, "Coding", "Write upload feature", "July 9, 2026", "high")];
-    const displayedTasks = displayTasks("All Tasks", dummyTasks);
-    const displayedCompleteTasks = displayCompleteTasks("Completed Tasks", dummyTasks)
+    const render = (user) => {
+        const displayedTasks = displayTasks("All Tasks", user.getTasks());
+        const displayedCompleteTasks = displayCompleteTasks("Completed Tasks", user.getCompleteTasks());
+        main.appendChild(displayedTasks);
+        main.appendChild(displayedCompleteTasks);
+    }
 
-    main.appendChild(displayedTasks);
-    main.appendChild(displayedCompleteTasks);
-
-    return main;
+    return { el: main, render };
 })();
