0eca79a8aec2769e3390d8d688e4e50143a1802a
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-03 00:45:23 +0800

    feat: enhance createChart function to accept parameters for dynamic chart creation

--- src/ui/components/Chart.js
+++ src/ui/components/Chart.js
@@ -1,5 +1,20 @@
 import Chart from 'chart.js/auto';
 
-export const createChart = () => {
-    
+export const createChart = (icon, title, option) => {
+    const { type, data, options } = option;
+    const div = document.createElement("div");
+    const heading = document.createElement("h1");
+    const canvas = document.createElement("canvas");
+
+    div.className = "chart";
+    heading.className = "chart__heading";
+    canvas.className = "chart__canvas";
+
+    title = "test";
+    heading.textContent = title;
+    new Chart(canvas, { type, data, options });
+
+    div.appendChild(heading);
+    div.appendChild(canvas);
+    return div;
 }
