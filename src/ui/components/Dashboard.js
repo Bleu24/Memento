4a970276ce6da950afb9fbc5ffc513c60d3e9103
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-04 01:15:12 +0800

    refactor: streamline Dashboard component by removing unused chart data and enhancing progress update logic

--- src/ui/components/Dashboard.js
+++ src/ui/components/Dashboard.js
@@ -1,70 +1,54 @@
+import { LocalRepository } from "../../repository/LocalRepository.js";
+import { UserService } from "../../services/UserService.js";
 import { createChart } from "./Chart.js";
 
 export const Dashboard = (function () {
     const div = document.createElement("div");
+    const charts = document.createElement("div");
+    const progressLabel = document.createElement("label");
+    const progress = document.createElement("div");
+    const progBar = document.createElement("progress");
+    const min = document.createElement("h2");
+    const max = document.createElement("h2");
+
+
+    // TODO: finalize updateProgress method
+    const updateProgress = (user) => {
+        if (user) {
+            const level = user.getLevel();
+            const threshold = user.getThreshold();
+            const runningXp = user.getRunningXp();
+            const leftoverXp = user.getLeftoverXp();
+
+            min.textContent = `${level}`;
+            max.textContent = `${level + 1}`;
+            progBar.max = threshold;
+            progBar.value = 20;
+        }
+    }
+
 
     div.className = "main__dashboard";
+    charts.className = "charts";
+    progressLabel.className = "progress__heading";
+    progress.className = "progress";
+    progBar.id = "xp";
+    progBar.className = "progress__bar";
+    min.className = "progress__min";
+    max.className = "progress__max";
 
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ];
-
-    div.appendChild(createChart(null, null, {
-        type: 'bar',
-        data: {
-            labels: data.map(row => row.year),
-            datasets: [
-                {
-                    label: 'Acquisitions by year',
-                    data: data.map(row => row.count)
-                }
-            ]
-        }
-    }));
-
-    div.appendChild(createChart(null, null, {
-        type: 'bar',
-        data: {
-            labels: data.map(row => row.year),
-            datasets: [
-                {
-                    label: 'Acquisitions by year',
-                    data: data.map(row => row.count)
-                }
-            ]
-        }
-    }));
-    div.appendChild(createChart(null, null, {
-        type: 'bar',
-        data: {
-            labels: data.map(row => row.year),
-            datasets: [
-                {
-                    label: 'Acquisitions by year',
-                    data: data.map(row => row.count)
-                }
-            ]
-        }
-    }));
-    div.appendChild(createChart(null, null, {
-        type: 'bar',
-        data: {
-            labels: data.map(row => row.year),
-            datasets: [
-                {
-                    label: 'Acquisitions by year',
-                    data: data.map(row => row.count)
-                }
-            ]
-        }
-    }));
+    progressLabel.htmlFor = "xp";
 
 
-    return div;
+    progressLabel.textContent = "Your Progress";
+
+
+    progress.append(min, progBar, max);
+
+    div.appendChild(progressLabel);
+    div.appendChild(progress);
+
+    updateProgress(UserService.loadLoggedInProfile(LocalRepository));
+
+    return { el: div, updateProgress };
 })();
