0cb7a7751793d6fbb256a665b42d893d732c60f2
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-06 02:10:31 +0800

    feat: enhance createDBTopCards function for improved card rendering

--- src/ui/components/Card.js
+++ src/ui/components/Card.js
@@ -19,3 +19,30 @@ export function createCard(icon, heading, body) {
 
     return card;
 }
+
+export function createDBTopCards(icon, label, data, text) {
+    const card = document.createElement("div");
+    const svg = createElement(icon);
+    const cardLabel = document.createElement("h2");
+    const cardText = document.createElement("p");
+    const cardHeading = document.createElement("div");
+    const cardBody = document.createElement("div");
+    const dataHolder = document.createElement("div");
+
+    card.className = "card";
+    cardLabel.className = "card__label";
+    cardText.className = "card__text";
+    cardHeading.className = "card__header";
+    svg.classList.add("card__icon");
+    cardBody.className = "card__body";
+
+    cardHeading.append(svg, cardLabel);
+    cardBody.append(dataHolder, cardText);
+    card.append(cardHeading, cardBody);
+
+    cardLabel.textContent = label;
+    dataHolder.textContent = data;
+    cardText.textContent = text;
+
+    return card;
+}
