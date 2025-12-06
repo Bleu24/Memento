21cb25a09dd0ab1db0c6de76bbbf95e775ba22e1
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-07 01:23:34 +0800

    fix: remove unnecessary break statement in MainPanel render function

--- src/ui/components/MainPanel.js
+++ src/ui/components/MainPanel.js
@@ -23,7 +23,6 @@ export const MainPanel = (function () {
             case "dashboard":
                 main.appendChild(Dashboard.el);
                 Dashboard.render(user);
-                break;
             case "tasks":
                 const userTasks = user.getTasks();
                 const userCompletedTasks = user.getCompletedTasks();
