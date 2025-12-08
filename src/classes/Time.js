323ff9da2824e905c189d0301af2dc77a46a9d3b
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-08 16:35:59 +0800

    feat: implement Time class with methods for current date and week calculations

---
+++ src/classes/Time.js
@@ -0,0 +1,38 @@
+import { format, differenceInCalendarDays, addDays, startOfWeek } from "date-fns";
+
+export class Time {
+
+    static #dateNow;
+    static #weekNow;
+
+    static get dateNow() {
+        this.#dateNow = new Date(Date.now());
+        return format(this.#dateNow, "dd-MM-yy");
+    }
+
+    static getDifferenceFromNow(date) {
+        return differenceInCalendarDays(this.#dateNow, date);
+    }
+
+    static getWeekOfDate(date) {
+        const week = [];
+        const start = startOfWeek(date);
+
+        for (let i = 0; i < 7; i++) {
+            week.push(addDays(start, i));
+        }
+
+        return [...week];
+    }
+
+    static get weekNow() {
+        this.#weekNow = [];
+        const start = startOfWeek(this.#dateNow);
+
+        for (let i = 0; i < 7; i++) {
+            week.push(addDays(start, i));
+        }
+
+        return [...this.#weekNow];
+    }
+}
