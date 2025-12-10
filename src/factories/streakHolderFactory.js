66e5cc74d52b01d0e1615cf0e5722db9d932a978
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-10 14:17:49 +0800

    feat: add createStreakHolder function to manage streak data

---
+++ src/factories/streakHolderFactory.js
@@ -0,0 +1,15 @@
+import { Time } from "../classes/Time.js";
+
+export const createStreakHolder = (dataObj) => {
+    let currentStreak = dataObj.currentStreak || 0;
+    let longestStreak = dataObj.longestStreak || 0;
+    let inStreak = dataObj.inStreak || false;
+
+    const checkStreakInWeek = () => {
+        
+    }
+
+    const logStreak = () => {
+        
+    }
+}
