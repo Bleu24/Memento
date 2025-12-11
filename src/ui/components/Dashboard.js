e9a99506781ac86c54fc646ddabc4c48e3cc962b
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-12 00:59:07 +0800

    add streakCard element to be appended in Dashboard

--- src/ui/components/Dashboard.js
+++ src/ui/components/Dashboard.js
@@ -2,7 +2,7 @@ import { LocalRepository } from "../../repository/LocalRepository.js";
 import { UserService } from "../../services/UserService.js";
 import { createDashboardCard } from "./Card.js";
 import { createChart } from "./Chart.js";
-import { createElement, Percent, ListTodo, SquareCheckBig } from "lucide";
+import { ListTodo, SquareCheckBig, Zap } from "lucide";
 
 export const Dashboard = (function () {
     const div = document.createElement("div");
@@ -17,6 +17,7 @@ export const Dashboard = (function () {
 
     let incompleteTasksCard = document.createElement('div');
     let completeTasksCard = document.createElement('div');
+    let streakCard = document.createElement('div');
 
 
     // TODO: finalize updateProgress method
@@ -32,13 +33,13 @@ export const Dashboard = (function () {
 
             incompleteTasksCard = createDashboardCard({ svg: ListTodo, options: { stroke: '#18F2B2' } }, "Unfinished Tasks", user.getTasks().length, user.getTasks().length ? "Work your ass off!" : "Good job! No work for today!");
             completeTasksCard = createDashboardCard({ svg: SquareCheckBig, options: { stroke: '#18F2B2' } }, "Finished Tasks", user.getCompletedTasks().length, user.getCompletedTasks().length ? "Good job! Keep on going!" : "Work your ass off!");
-            
+            streakCard = createDashboardCard({ svg: Zap, options: { stroke: '#18F2B2' } }, "Streaks");
 
             if (cards.hasChildNodes()) return;
 
             cards.appendChild(incompleteTasksCard);
             cards.appendChild(completeTasksCard);
-            cards.appendChild(completeTasksCard);
+            cards.appendChild(streakCard);
 
 
         }
