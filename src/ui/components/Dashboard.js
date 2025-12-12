7151eda6f4431c4a1a4e96aa8d2e4ff7ab10a9d0
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-12 17:36:23 +0800

    remove unused imports and add streakCard to be appended in Dashboard module

--- src/ui/components/Dashboard.js
+++ src/ui/components/Dashboard.js
@@ -1,8 +1,6 @@
-import { LocalRepository } from "../../repository/LocalRepository.js";
-import { UserService } from "../../services/UserService.js";
 import { createDashboardCard } from "./Card.js";
-import { createChart } from "./Chart.js";
 import { ListTodo, SquareCheckBig, Zap } from "lucide";
+import { createDayNodes } from "./DayNodes.js";
 
 export const Dashboard = (function () {
     const div = document.createElement("div");
@@ -33,15 +31,13 @@ export const Dashboard = (function () {
 
             incompleteTasksCard = createDashboardCard({ svg: ListTodo, options: { stroke: '#18F2B2' } }, "Unfinished Tasks", user.getTasks().length, user.getTasks().length ? "Work your ass off!" : "Good job! No work for today!");
             completeTasksCard = createDashboardCard({ svg: SquareCheckBig, options: { stroke: '#18F2B2' } }, "Finished Tasks", user.getCompletedTasks().length, user.getCompletedTasks().length ? "Good job! Keep on going!" : "Work your ass off!");
-            streakCard = createDashboardCard({ svg: Zap, options: { stroke: '#18F2B2' } }, "Streaks");
+            streakCard = createDashboardCard({ svg: Zap, options: { stroke: '#18F2B2' } }, "Streaks", createDayNodes(), "FUCK");
 
             if (cards.hasChildNodes()) return;
 
             cards.appendChild(incompleteTasksCard);
             cards.appendChild(completeTasksCard);
             cards.appendChild(streakCard);
-
-
         }
     }
 
