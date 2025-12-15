40ddc676a2a9b5aaa9ef04dc609e052b53ad3bd2
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-16 00:41:39 +0800

    feat: add mode for displayTaskModal for conditional rendering and implement edit logic for task

--- src/ui/components/Tasks.js
+++ src/ui/components/Tasks.js
@@ -1,14 +1,13 @@
-import { createElement, Dot, Ellipsis, CirclePlus, BusFront } from "lucide";
+import { createElement, Dot, Ellipsis, CirclePlus } from "lucide";
 import { MainPanel } from '../components/MainPanel.js';
-import { Task } from "../../classes/Task";
 import { UserService } from "../../services/UserService";
 import { LocalRepository } from "../../repository/LocalRepository";
-import { UIService } from "../../services/UIService.js";
 
-const displayTaskModal = () => {
+const displayTaskModal = (mode) => {
     const bg = document.createElement('div');
     const formContainer = document.createElement('div');
     const form = document.createElement('form');
+    const formTitle = document.createElement('h1');
     const row1 = document.createElement('div');
     const row2 = document.createElement('div');
     const row3 = document.createElement('div');
@@ -21,6 +20,8 @@ const displayTaskModal = () => {
     bg.className = "modal__overlay";
     formContainer.className = "formContainer";
     form.className = "taskForm";
+    formTitle.className = "taskForm__title";
+
 
     // For row 1
     const titleLabel = document.createElement('label');
@@ -30,7 +31,7 @@ const displayTaskModal = () => {
     titleInput.id = "title";
     titleInput.name = "title";
     titleInput.type = "text";
-    titleInput.placeholder = "Coding: To-Do App";
+    titleInput.required = true;
     row1.append(titleLabel, titleInput);
 
 
@@ -42,7 +43,6 @@ const displayTaskModal = () => {
     descInput.id = "description";
     descInput.name = "description";
     descInput.type = "text";
-    descInput.placeholder = "Debugging line 43 col 23";
     row2.append(descLabel, descInput);
 
     // row 3
@@ -52,8 +52,10 @@ const displayTaskModal = () => {
     dateLabel.setAttribute("for", "date");
     dateInput.id = "date";
     dateInput.name = "date";
-    dateInput.type = "date";
+    dateInput.type = "text";
+    dateInput.required = true;
     row3.append(dateLabel, dateInput);
+
 
     // row 4
     fieldSet.appendChild(legend);
@@ -72,22 +74,39 @@ const displayTaskModal = () => {
         radioGroup.append(prioLabel, prioInput);
         fieldSet.append(radioGroup);
         row4.append(fieldSet);
+        prioInput.required = true;
+    }
+    switch (mode) {
+        case "create":
+            submit.textContent = "Create";
+            formTitle.textContent = "Create Task";
+            break;
+        case "edit":
+            submit.textContent = "Edit";
+            formTitle.textContent = "Edit Task";
+            break;
     }
 
-    submit.textContent = "Create Task";
-
-    form.append("Create Task", row1, row2, row3, row4, submit);
+    form.append(formTitle, row1, row2, row3, row4, submit);
     formContainer.appendChild(form);
     bg.appendChild(formContainer);
 
+    // Listeners
+
     bg.addEventListener('click', (e) => {
         if (e.target.closest(".modal__overlay")) {
-            bg.remove()
+            bg.remove();
         }
     })
 
     form.addEventListener('click', (e) => {
         e.stopPropagation();
+
+        if (e.target.closest("input#date")) {
+            dateInput.setAttribute("type", "date");
+        } else {
+            dateInput.setAttribute("type", "text");
+        }
     })
 
     form.addEventListener('submit', (e) => {
@@ -96,24 +115,87 @@ const displayTaskModal = () => {
         const taskInfo = Array.from(formData.values());
         const user = UserService.loadLoggedInProfile(LocalRepository);
 
-        const task = new Task(null, ...taskInfo);
+        switch (mode) {
+            case "create":
+                const newTask = UserService.createTaskForUser(user, taskInfo);
+                UserService.assignTask(newTask, user);
+                break;
+            case "edit":
+                const taskId = form.id
+                const retrievedTask = UserService.retrieveTask(taskId, user)
 
-        UserService.assignTask(task, user);
+                UserService.editTask(
+                    retrievedTask,
+                    {
+                        title: taskInfo[0],
+                        description: taskInfo[1],
+                        dueDate: taskInfo[2],
+                        priority: taskInfo[3]
+                    },
+                    user
+                );
+
+                break;
+            default:
+                break;
+        }
+
+
         UserService.saveProfileToStorage(LocalRepository, user);
+        Tasks.render(user);
 
-        bg.remove();
+        setTimeout(() => {
+            bg.remove();
+        }, 1000);
+
     });
 
     return bg;
 }
 
 const handleClick = (e) => {
+    let taskModal = "";
+    const taskItem = e.target.closest(".taskList__item");
+    const taskBtn = e.target.closest(".task__btn");
 
-    if (e.target.closest(".task__btn")) {
-        const taskModal = displayTaskModal();
+    if (taskBtn) {
+        taskModal = displayTaskModal("create");
         MainPanel.el.appendChild(taskModal);
+
+        const titleInput = document.querySelector("input#title");
+        const descInput = document.querySelector("input#description");
+        const dateInput = document.querySelector("input#date");
+        titleInput.placeholder = "Coding: To-Do App";
+        descInput.placeholder = "Debugging line 43 col 23";
+        dateInput.placeholder = "08/19/2025";
     }
-    return
+
+    if (taskItem) {
+        const edit = taskItem.querySelector(".edit");
+        if (edit) {
+            taskModal = displayTaskModal("edit");
+            MainPanel.el.appendChild(taskModal);
+
+            const user = UserService.loadLoggedInProfile(LocalRepository);
+            const id = taskItem.querySelector("input[type='checkbox']").id;
+            const task = UserService.retrieveTask(id, user);
+
+            const form = document.querySelector("form");
+            const titleInput = document.querySelector("input#title");
+            const descInput = document.querySelector("input#description");
+            const dateInput = document.querySelector("input#date");
+            const radioInput = document.querySelector(`input#${task.priority}`);
+
+
+            titleInput.value = task.title;
+            descInput.value = task.description;
+            dateInput.value = task.dueDate;
+            radioInput.checked = true;
+            form.id = id;
+        }
+    }
+
+    return;
 }
 
 export const Tasks = (function () {
@@ -142,7 +224,7 @@ export const Tasks = (function () {
         ['high', 'red']
     ];
 
-    
+
     const render = (user) => {
         const tasks = user.getTasks();
         const tasksNodes = []
@@ -159,7 +241,7 @@ export const Tasks = (function () {
             //left side elements
             const checkBox = document.createElement('input');
             checkBox.type = 'checkbox';
-            checkBox.id = 'user__task';
+            checkBox.id = task.id;
             checkBox.name = 'user__task';
             const taskTitle = document.createElement('label');
             taskTitle.textContent = task.title;
@@ -171,6 +253,8 @@ export const Tasks = (function () {
 
             const prio = createElement(Dot);
             const edit = createElement(Ellipsis);
+
+            edit.classList.add("edit");
 
 
             for (const arr of prioMap) {
