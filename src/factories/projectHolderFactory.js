1a43df46fd7afc8eb147e280397a657787700a65
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-12-29 22:52:35 +0800

    feat: add getProject using id lookup and refactor serialized method to expose project id

--- src/factories/projectHolderFactory.js
+++ src/factories/projectHolderFactory.js
@@ -1,10 +1,20 @@
+import { Project } from '../classes/Project.js'
+
 export const createProjectHolder = () => {
 
     let projects = [];
 
     const serialized = (project) => {
-        const { addTask, serialize, createTask, removeTask, editTask, completeTask, getTasks, getCompletedTasks, ...rest } = project;
-        return structuredClone(rest);
+        const { addTask, serialize, createTask, removeTask, editTask, completeTask, getTask, getTasks, getCompletedTasks, ...rest } = project;
+
+        const serializedProject = { id: project.id ? project.id : crypto.randomUUID(), ...rest };
+
+        return serializedProject;
+    }
+
+    const createProject = (id, title, desc) => {
+        if (id) return new Project(id, title, desc);
+        else return new Project(null, title, desc);
     }
 
     const addProject = (project) => {
@@ -15,7 +25,20 @@ export const createProjectHolder = () => {
         projects = projects.filter(project => project.id !== selectedProject.id);
     }
 
+    const getProject = (id) => {
+        if (!id) {
+            console.log("Invalid Project Id");
+            return;
+        }
+
+        for (const project of projects) {
+            if (project.id === id) {
+                return project;
+            }
+        }
+    }
+
     const getProjects = () => structuredClone(projects);
 
-    return { addProject, removeProject, getProjects };
+    return { createProject, addProject, removeProject, getProject, getProjects };
 }
