5dbeb762c0ef8b3e7ba09245c73a69d51339d70d
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-01-05 17:59:21 +0800

    refactor: remove unused renderTasksFromProject function from UIService

--- src/services/UIService.js
+++ src/services/UIService.js
@@ -9,18 +9,6 @@ export const UIService = (function () {
         el.render(props);
     };
 
-    const renderTasksFromProject = (el, props) => {
-
-        if (typeof props !== "object") {
-            console.error("props is not an object", props);
-            return;
-        }
-        el.render(props);
-    }
-
-
-
-
     return { render }
 
 })();
