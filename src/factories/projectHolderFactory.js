137555ff347d14a2caea4f2bcb0094961c23b342
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-02-06 00:43:53 +0800

    feat: add undoCompletion method to project serialization

--- src/factories/projectHolderFactory.js
+++ src/factories/projectHolderFactory.js
@@ -5,7 +5,7 @@ export const createProjectHolder = () => {
     let projects = [];
 
     const serialized = (project) => {
-        const { addTask, serialize, createTask, removeTask, editTask, completeTask, getTask, getTasks, getCompletedTasks, ...rest } = project;
+        const { addTask, serialize, createTask, removeTask, editTask, completeTask, undoCompletion, getTask, getTasks, getCompletedTasks, ...rest } = project;
 
         const serializedProject = { id: project.id ? project.id : crypto.randomUUID(), ...rest, tasks: project.getTasks() };
 
