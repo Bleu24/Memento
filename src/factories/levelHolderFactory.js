f1cb11534611614bd3193a769930cbd453e3dac9
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-02-09 00:01:19 +0800

    refactor: simplify XP computation and remove unused variables in levelHolderFactory

--- src/factories/levelHolderFactory.js
+++ src/factories/levelHolderFactory.js
@@ -2,43 +2,29 @@ export const createLevelHolder = (xpHolder) => {
 
     const MAX_LEVEL = 50; //tweakable
     let threshold = 100;
-    let leftoverXp = 0;
-    let runningXp = 0;
     let level = 1;
 
-    const onLevelUp = () => {
-        const isMaxLevel = level >= MAX_LEVEL;
-
-        if (isMaxLevel) return;
-
-        threshold += 100;
-        level++;
-        leftoverXp = 0; // not sure if I should be setting this to zero
-    }
-
-    // TODO: polish computation of xp
     const computeXP = () => {
-        const xp = xpHolder.getXP();
-        if (xp >= threshold) {
-            leftoverXp = xp - threshold;
-            runningXp = runningXp + xp + leftoverXp;
-            xpHolder.setXP(runningXp);
-            onLevelUp();
-            return { runningXp, leftoverXp };
-        } else {
-            runningXp += xp;
-            xpHolder.setXP(runningXp);
-            return runningXp;
+        let currentXP = xpHolder.getXP();
+
+
+        while (currentXP >= threshold && level < MAX_LEVEL) {
+            currentXP -= threshold;
+            level++;
+            threshold += 100;
         }
+
+
+        if (level >= MAX_LEVEL) currentXP = Math.min(currentXP, threshold);
+
+        return currentXP; // This is your new "leftover" or current XP
     }
 
 
     // TODO: polish apis
-    const getLeftoverXp = () => leftoverXp
-    const getRunningXp = () => runningXp;
     const getThreshold = () => threshold;
     const getLevel = () => level;
 
 
-    return { getLevel, getThreshold, getRunningXp, getLeftoverXp, computeXP };
+    return { getLevel, getThreshold, computeXP };
 }
