abf010481e6279592ba547adbfa60e9de715c1d3
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-02-09 20:12:31 +0800

    fix: update createLevelHolder to accept initial level and improve threshold computation

--- src/factories/levelHolderFactory.js
+++ src/factories/levelHolderFactory.js
@@ -1,29 +1,37 @@
-export const createLevelHolder = (xpHolder) => {
+export const createLevelHolder = (xpHolder, lvl) => {
 
     const MAX_LEVEL = 50; //tweakable
-    let threshold = 100;
-    let level = 1;
+    let level = lvl;
+
+    const computeThreshold = () => {
+        const baseXP = 100;
+        const exponent = 1.3;
+
+        return Math.floor(baseXP * Math.pow(level, exponent));
+    }
 
     const computeXP = () => {
         let currentXP = xpHolder.getXP();
-
+        let threshold = getThreshold();
 
         while (currentXP >= threshold && level < MAX_LEVEL) {
             currentXP -= threshold;
             level++;
-            threshold += 100;
+            threshold = computeThreshold();
         }
 
+        while (currentXP > 0 )
 
         if (level >= MAX_LEVEL) currentXP = Math.min(currentXP, threshold);
 
-        return currentXP; // This is your new "leftover" or current XP
+        return currentXP;
     }
 
 
     // TODO: polish apis
-    const getThreshold = () => threshold;
+    const getThreshold = () => computeThreshold();
     const getLevel = () => level;
+
 
 
     return { getLevel, getThreshold, computeXP };
