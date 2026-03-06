298f41bb0069a9668ef69b401a5ef8591df61cbd
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-06 17:44:46 +0800

    feat: update Streak class to ensure date is a valid Date object and change date format to yyyy-MM-dd

--- src/classes/Streak.js
+++ src/classes/Streak.js
@@ -1,27 +1,27 @@
 import { format } from "date-fns";
-import { Time } from "./Time";
 
 export class Streak {
     date;
     tasksCompleted = 0;
 
     constructor(date, tasksCompleted = 0) {
-        // Ensure date is a valid Date object, otherwise default to today
-        this.date = date instanceof Date ? date : Time.dateNow;
+        this.date = date;
         this.tasksCompleted = tasksCompleted;
     }
 
-    // Dynamically calculate status based on your 3-task rule
     get status() {
         return this.tasksCompleted >= 3;
     }
 
-    // Method to increment tasks for this specific day
     logTask() {
         this.tasksCompleted++;
     }
 
+    undoLog() {
+        this.tasksCompleted--;
+    }
+
     get dateFormatted() {
-        return format(this.date, "dd-MM-yy");
+        return format(this.date, "yyyy-MM-dd");
     }
 }
