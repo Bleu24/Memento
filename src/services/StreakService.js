f6277bc890904e5c8258ace7ec52f14c6b4b56aa
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-06 17:45:07 +0800

    feat: implement retrieveStreak, logActivity, and undoLogActivity methods in StreakService

--- src/services/StreakService.js
+++ src/services/StreakService.js
@@ -1,11 +1,28 @@
 export const StreakService = (function () {
 
     const addStreak = (user, streak) => {
-        
+        user.addStreak(streak);
+    }
+
+    const retrieveStreak = (user) => {
+        return user.getStreaks();
+    }
+
+    const logActivity = (user) => {
+        user.logActivity();
+    }
+
+    const undoLogActivity = (user) => {
+        user.undoLog();
     }
 
 
-
+    return {
+        addStreak,
+        retrieveStreak,
+        logActivity,
+        undoLogActivity
+    };
 
 
 })();
